b2a09c83300b1f45f9b4d7febc0765e4e3462c2d
Manuel Zambrano <mzambrano595@gmail.com>
2018-05-23 19:32:15 -0400

    agragando validacion de campos de registro con Regex

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager 
+from django.core.validators import RegexValidator
 
 
 ESTADO_USUARIO = (
@@ -14,6 +15,7 @@ SEXO = (
     ('F', 'Femenino'),
 )
 
+
 class CustomUserManager(UserManager):
     pass
 
@@ -21,17 +23,26 @@ class CustomUser(AbstractUser):
 	objects = CustomUserManager()
 
 class CompleteUser(CustomUser):
-	cedula = models.CharField(max_length=15, unique= True)
+	cedula = models.CharField(
+        max_length=11, 
+        unique= True,
+        validators=[RegexValidator(regex="^[V|E]{1}[0]{1}[0-9]{7,9}$",message="Formato de Cedula invalido , ej: V0123456")],
+    )
 	estado = models.CharField(
         max_length=1,
         choices=ESTADO_USUARIO,
         default='T',
     )
 	sexo = models.CharField(max_length=1, choices=SEXO)
-	cod_area = models.CharField(max_length=4)
-	num_telefono = models.CharField(max_length=8)
-	direccion = models.TextField(max_length=150)
+	cod_area = models.CharField(max_length=4,
+        validators=[RegexValidator(regex="^[0]{1}[2-4]{1}[1-9]{1}[1-9]{1}$",message="Codigo de Area invalido , ej: 0274")],
+    )
+	num_telefono = models.CharField(max_length=7)
+	direccion = models.CharField(max_length=150)
 	carrera = models.CharField(max_length=100, blank=True)
     
     
 # Create your models here.
+'''validators=[RegexValidator
+                                      (regex="^[0-9]{10}$",
+message="Invalid format for CI")]'''
