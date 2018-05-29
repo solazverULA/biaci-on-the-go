13036f79079cbda4a3911d5051e94aa726077703
Yenifer Ramírez <yeniferramirez11@gmail.com>
2018-05-29 03:22:00 -0400

    Buscador en Menu de cualquier recurso, solo por coincidencia de titulo

--- libro/urls.py
+++ libro/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('libros/', views.LibrosVista.as_view(), name='libros'),
     #path('libros/', views.LibrosVista, name='libro'),
     path('<pk>/libros/', views.EjemplaresVista.as_view(), name='ejemplar'),
+    path('consulta/', views.ConsultaTodo.as_view(), name='consulta'),
 ]
--- libro/views.py
+++ libro/views.py
@@ -7,7 +7,11 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
 
 
-from .models import Libro, Autor, Materia, Ejemplar, Biblioteca, Idioma
+from .models import Libro
+
+from tesis.models import Tesis
+
+from revista.models import Revista
 
 
 from .forms import ConsultaLibroForm
@@ -92,17 +96,53 @@ class EjemplaresVista(View):
             'ejemplar.html',
             context={'ejemplar': libro, }
         )
-    """def ejemplar(request, pk):
-        try:
-            libro_id = Libro.objects.get(pk=pk)
-        except Libro.DoesNotExist:
-            raise Http404("Este Libro no existe")
 
-        # book_id=get_object_or_404(Book, pk=pk)
 
-        return render(
-            request,
-            'ejemplar.html',
-            context={'ejemplar': libro_id, }
-        )
-    """
+class LibrosVista(View):
+
+    def post(self, request, *args, **kwargs):
+
+        palabra = request.POST['titulo']
+
+        biblioteca = request.POST['biblioteca']
+        por = request.POST['buscar_por']
+
+        if por == '0' and biblioteca == '':
+            consulta = Libro.objects.filter(titulo__icontains=palabra)
+
+        if por == '1' and biblioteca == '':
+            consulta = Libro.objects.filter(autor__nombre__icontains=palabra)
+
+        if por == '2' and biblioteca == '':
+            consulta = Libro.objects.filter(isbn__icontains=palabra)
+
+        if por == '3' and biblioteca == '':
+            consulta = Libro.objects.filter(cota__icontains=palabra)
+
+        if por == '0' and biblioteca != '':
+            consulta = Libro.objects.filter(titulo__icontains=palabra, biblioteca=biblioteca)
+
+        if por == '1' and biblioteca != '':
+            consulta = Libro.objects.filter(autor__nombre__icontains=palabra, biblioteca=biblioteca)
+
+        if por == '2' and biblioteca != '':
+            consulta = Libro.objects.filter(isbn__icontains=palabra, biblioteca=biblioteca)
+
+        if por == '3' and biblioteca != '':
+            consulta = Libro.objects.filter(cota__icontains=palabra, biblioteca=biblioteca)
+
+        return TemplateResponse(request, 'libros.html', {'consulta': consulta, 'palabra':palabra})
+
+
+class ConsultaTodo(View):
+
+    def post(self, request, *args, **kwargs):
+
+        context = {
+            'palabra': request.POST['palabra'],
+            'libro': Libro.objects.filter(titulo__icontains=request.POST['palabra']),
+            'tesis': Tesis.objects.filter(titulo__icontains=request.POST['palabra']),
+            'revista': Revista.objects.filter(titulo__icontains=request.POST['palabra']),
+        }
+
+        return TemplateResponse(request, 'consulta.html', context)
