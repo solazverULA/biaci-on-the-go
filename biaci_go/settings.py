87833deab55629481f072e4c3aa9b7311e958239
Yenifer Ramírez <yeniferramirez11@gmail.com>
2018-05-29 00:24:30 -0400

    Parametro debug a FALSE para despliegue

--- biaci_go/settings.py
+++ biaci_go/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mf@#&%u#70ya%s@emh=68(z9690a@3)y5ejw06ie+w-tq_t)g('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
