2d7b0c8a2e6ccdb95ea3406d57bcd81a2f942514
Manuel Zambrano <mzambrano595@gmail.com>
2018-06-10 14:06:18 -0400

    recuperacion de contraseña

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('users/signup/', views.SignUp.as_view(), name='signup'),
     path('', views.HomePageView.as_view(), name='home'),
     path('perfil/', views.PerfilView, name='perfil'),
+    
 ]
