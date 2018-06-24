55846464a12f9eb3932aca717f81850867548f9c
Manuel Zambrano <mzambrano595@gmail.com>
2018-06-24 14:02:12 -0400

    vista para el ingreso de cedula incorrecta

--- reserva/urls.py
+++ reserva/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
 
-    path('list/', views.ErrorReserva.as_view(), name='error_reserva'),
-    path('error/', views.ReservaLibros.as_view(), name='lista'),
+    path('error/', views.ErrorReserva.as_view(), name='error_reserva'),
+    path('list/', views.ReservaLibros.as_view(), name='lista'),
     path('<int:id_ejemplar>', views.Reservar, name='reservar'),
     path('(?<id_reserva>\d+)$', views.Reserva_delete, name='eliminar'),
 ]
